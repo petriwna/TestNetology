5de989b032a730e4868c169a34c6b5842b8572d3
Anton <a.k.a.a@bk.ru>
2021-02-26 18:15:01 +0200

    homework_4 netology

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,14 +1,26 @@
 package com.example.test
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.wolfram.alpha.WAEngine
 import com.wolfram.alpha.WAPlainText
+import java.lang.Exception
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var textToSpeech: TextToSpeech
+    var speechRequest = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,6 +32,53 @@ class MainActivity : AppCompatActivity() {
 
         searchButton.setOnClickListener {
             askWolfram(questionInput.text.toString())
+        }
+
+        val speakButton = findViewById<Button>(R.id.speak_button)
+        speakButton.setOnClickListener {
+            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            intent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.US)
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "What do you want to know?")
+            try {
+                startActivityForResult(intent, 1)
+            } catch (a: Exception) {
+                Toast.makeText(
+                    applicationContext,
+                    "Sorry your device not supported",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        val answerOutput = findViewById<TextView>(R.id.answer_output)
+        textToSpeech.language = Locale.US
+
+        findViewById<FloatingActionButton>(R.id.read_answer).setOnClickListener {
+            val answer = answerOutput.text.toString()
+            textToSpeech.speak(answer, TextToSpeech.QUEUE_ADD, null, speechRequest.toString())
+            speechRequest +=1
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK && data != null) {
+                val result: ArrayList<String>? = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS
+                )
+                val question: String? = result?.get(0)
+
+                if (question != null){
+                    findViewById<TextView>(R.id.question_input).text = question
+
+                    askWolfram(question)
+                }
+            }
         }
     }
 
@@ -55,6 +114,10 @@ class MainActivity : AppCompatActivity() {
                                     if (element is WAPlainText) {
                                         Log.d("wolfram", element.text)
                                         answerText.text = element.text
+
+                                        val answer = findViewById<TextView>(R.id.answer_output).text.toString()
+                                        textToSpeech.speak(answer, TextToSpeech.QUEUE_ADD, null, speechRequest.toString())
+                                        speechRequest +=1
                                     }
                                 }
                             }
